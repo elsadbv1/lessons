f203d97d586a40614ca939a12245ce09f07d2f1c
elshad bayramov <elsadbv@gmail.com>
2022-06-07 23:31:55 +0400

    added 2nd homework

---
+++ HomeWork/04.06.2022/HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string StandardPascal = ("ISO 7185");
+            string camelStandard = ("nameSurname");
+            double DoubleHungarionStandard = 779;
+            string Underscore_Standard = ("Bayramov_Elshad");
+            string UPPERSCORE_STANDARD = ("x"); // Adətən sabit dəyərlər üçün istifadə edilir
+
+
+
+        }
+    }
+}
