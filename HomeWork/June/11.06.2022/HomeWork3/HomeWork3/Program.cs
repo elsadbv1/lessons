ec2a5bb6105a5b9d49892b202b632f36b4367bb6
elshad bayramov <elsadbv@gmail.com>
2022-06-14 17:09:13 +0400

    added homework 3

--- HomeWork/June/11.06.2022/HomeWork3/HomeWork3/Program.cs
+++ HomeWork/June/11.06.2022/HomeWork3/HomeWork3/Program.cs
@@ -14,6 +14,63 @@ namespace HomeWork3
                          edildikdən sonra alınan nəticəyə 1 toplayacaq və ya çıxacaqsız. Vurmada bölmədə isə, alınan nəticəni 20'ə vurun və ya bölün.
                */
 
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            string a1 = "rəqəm";
+
+            Console.WriteLine("10 fərqli rəqəm daxil edin");
+            
+            Console.WriteLine("1-ci " + a1 + ":");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("2-ci " + a1 + ":");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("3-cü " + a1 + ":");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("4-cü " + a1 + ":");
+            int d = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("5-ci " + a1 + ":");
+            int e = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("6-cı " + a1 + ":");
+            int f = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("7-ci " + a1 + ":");
+            int g = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("8-ci " + a1 + ":");
+            int h = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("9-cu " + a1 + ":");
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("10-cu " + a1 + ":");
+            int l = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine(a + b + c + d + f + g + h + k + l +1);
+            Console.WriteLine(a - b - c - d - f - g - h - k - l - 1);
+            Console.WriteLine(a * b * c * d * f * g * h * k * 20);
+            Console.WriteLine(a / b / c / d / f / g / h / k / 20);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         }
